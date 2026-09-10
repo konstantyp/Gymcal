129f9e49ccee0ff57fdbdabc774fe10ef5aae40d
konstantyp <konstantyp@users.noreply.github.com>
2026-09-10 12:38:01 +0000

    Bump version to 1.1.8 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.konstantyp.gymcal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.1.7"
+        versionCode = 10
+        versionName = "1.1.8"
         setProperty("archivesBaseName", "Liftloq")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
